d947f0422d535dd65de1ad3efdaba01fdb986d14
SMsajjadSM <sajjadsiadati1390@gmail.com>
2025-01-30 15:23:47 +0330

    modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,19 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      backgroundImage: {
+        "hero-pattern": "url('../images/form-pattern.png')",
+      },
       colors: {
         silver: {
           50: "#a6a6a6",
+          100: "#161718",
+          150: "#1b1c1d",
+          200: "hsla(0, 0%, 100%, 0.1)",
+          250: "#0e0d0c",
+          300:"#141515",
+          350:"#121111"
+
         },
         gold: { 50: "#e4c590" },
       },
@@ -36,7 +46,6 @@ module.exports = {
         "0%": { transform: "translateY(0)" },
         " 50%": { transform: "translateY(30px)" },
         " 100%": { transform: "translateY(0) " },
-
       },
       rotate: {
         "0%": { transform: "rotate(0deg)" },
